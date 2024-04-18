9bcb3525b7c7344c320ff654ceb59cef50627ac3
AdrianMa1996 <adrian.marxreiter@web.de>
2024-04-19 01:48:42 +0200

    ManagePlayers Design verbessert

--- KnockKnockApp/Platforms/Android/MainApplication.cs
+++ KnockKnockApp/Platforms/Android/MainApplication.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
 
 namespace KnockKnockApp
 {
@@ -11,6 +12,16 @@ namespace KnockKnockApp
         {
         }
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        protected override MauiApp CreateMauiApp()
+        {
+            // Remove Entry control underline
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (h, v) =>
+            {
+                h.PlatformView.BackgroundTintList =
+                    Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+            });
+
+            return MauiProgram.CreateMauiApp();
+        }
     }
 }
