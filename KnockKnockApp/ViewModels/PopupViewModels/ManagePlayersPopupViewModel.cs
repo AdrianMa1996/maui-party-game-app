5b9962b28be24507b70f2f37caa160f0e1b39301
AdrianMa1996 <adrian.marxreiter@web.de>
2024-04-28 16:52:04 +0200

    ManagePlayersPopup vervollständigen und designen

--- KnockKnockApp/ViewModels/PopupViewModels/ManagePlayersPopupViewModel.cs
+++ KnockKnockApp/ViewModels/PopupViewModels/ManagePlayersPopupViewModel.cs
@@ -1,7 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KnockKnockApp.Models;
 using KnockKnockApp.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +15,39 @@ namespace KnockKnockApp.ViewModels.PopupViewModels
 {
     public partial class ManagePlayersPopupViewModel : ObservableObject
     {
-        public ManagePlayersPopupViewModel(ILocalizationService localizationService)
+        private readonly IPlayerManagementService _playerManagementService;
+
+        public ManagePlayersPopupViewModel(ILocalizationService localizationService, IPlayerManagementService playerManagementService)
         {
             LocalizationService = localizationService;
+            _playerManagementService = playerManagementService;
+
+            AllPlayers = _playerManagementService.GetAllPlayers();
         }
 
         [ObservableProperty]
         public ILocalizationService localizationService;
+
+        [ObservableProperty]
+        public ObservableCollection<Player> allPlayers;
+
+        [ObservableProperty]
+        public string spielerNameEntryText = string.Empty;
+
+        [RelayCommand]
+        public void AddPlayer()
+        {
+            var playerName = SpielerNameEntryText;
+            _playerManagementService.AddPlayer(playerName);
+            SpielerNameEntryText = string.Empty;
+        }
+
+        [RelayCommand]
+        public void RemovePlayer(object commandParameter)
+        {
+            var player = (Player)commandParameter;
+            var playerId = player.Id;
+            _playerManagementService.RemovePlayer(playerId);
+        }
     }
 }
--- KnockKnockApp/Views/PopupViews/ManagePlayersPopupView.xaml.cs
+++ KnockKnockApp/Views/PopupViews/ManagePlayersPopupView.xaml.cs
@@ -1,4 +1,6 @@
+using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Views;
+using KnockKnockApp.Services;
 using KnockKnockApp.ViewModels.PopupViewModels;
 
 namespace KnockKnockApp.Views.PopupViews;
@@ -6,12 +8,29 @@ namespace KnockKnockApp.Views.PopupViews;
 public partial class ManagePlayersPopupView : Popup
 {
     private readonly ManagePlayersPopupViewModel _viewModel;
+    private readonly IDeviceOrientationService _deviceOrientationService;
 
-    public ManagePlayersPopupView(ManagePlayersPopupViewModel viewModel)
+    public ManagePlayersPopupView(ManagePlayersPopupViewModel viewModel, IDeviceOrientationService deviceOrientationService)
 	{
         BindingContext = _viewModel = viewModel;
+        _deviceOrientationService = deviceOrientationService;
         InitializeComponent();
-	}
+        Opened += OnPopupOpened;
+        Closed += OnPopupClosed;
+    }
+
+    private void OnPopupOpened(object? sender, PopupOpenedEventArgs e)
+    {
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Portrait);
+        SpielerNameEntry.IsEnabled = true;
+        SpielerNameEntry.Focus();
+    }
+
+    private void OnPopupClosed(object? sender, PopupClosedEventArgs e)
+    {
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Landscape);
+        SpielerNameEntry.IsEnabled = false;
+    }
 
     private void CloseButton_Clicked(object sender, EventArgs e)
     {
