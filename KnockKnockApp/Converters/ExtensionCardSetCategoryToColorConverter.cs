0f7e6e03649ba06287e6767115bc73bcc77b946e
AdrianMrx <adrian.marxreiter@hs-augsburg.de>
2024-11-07 15:23:44 +0100

    Füge Extension ContentView-Selector hinzu und den ExtensionCardSerCategoryToColorConverter

---
+++ KnockKnockApp/Converters/ExtensionCardSetCategoryToColorConverter.cs
@@ -0,0 +1,74 @@
+﻿using KnockKnockApp.Models.Database;
+using System.Globalization;
+
+namespace KnockKnockApp.Converters
+{
+    public class ExtensionCardSetCategoryToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ExtensionCardSetCategory)
+            {
+                ExtensionCardSetCategory category = (ExtensionCardSetCategory)value;
+                Color color;
+
+                switch (category)
+                {
+                    case ExtensionCardSetCategory.ThemedGamePreparation:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.ThemedGame:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.ThemedGameCompletion:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.RhymingGamePreparation:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.RhymingGame:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.RhymingGameCompletion:
+                        color = GetColorFromResources("GreenCardColor");
+                        break;
+                    case ExtensionCardSetCategory.MimePreparation:
+                        color = GetColorFromResources("PinkCardColor");
+                        break;
+                    case ExtensionCardSetCategory.Mime:
+                        color = GetColorFromResources("PinkCardColor");
+                        break;
+                    case ExtensionCardSetCategory.MimeCompletion:
+                        color = GetColorFromResources("PinkCardColor");
+                        break;
+                    case ExtensionCardSetCategory.GameRule:
+                        color = GetColorFromResources("RedCardColor");
+                        break;
+                    default:
+                        color = GetColorFromResources("GrayCardColor");
+                        break;
+                }
+
+                return color;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private Color GetColorFromResources(string key)
+        {
+            var color = Colors.White;
+            if (App.Current.Resources.TryGetValue(key, out var colorvalue))
+            {
+                color = (Color)colorvalue;
+            }
+
+            return color;
+        }
+    }
+}
--- KnockKnockApp/Views/CardGameExtension/ExtensionTimeBombPreparationView.xaml.cs
+++ KnockKnockApp/Views/CardGameExtension/ExtensionTimeBombPreparationView.xaml.cs
@@ -1,9 +1,71 @@
+using CommunityToolkit.Mvvm.Input;
+using KnockKnockApp.Models.Database;
+using KnockKnockApp.Models.DTOs;
+
 namespace KnockKnockApp.Views.CardGameExtension;
 
-public partial class ExtensionTimeBombPreparationView : ContentPage
+public partial class ExtensionTimeBombPreparationView : ContentView
 {
-	public ExtensionTimeBombPreparationView()
+    public static readonly BindableProperty CurrentCardProperty = BindableProperty.Create(nameof(CurrentCard), typeof(ExtensionCardDto), typeof(ExtensionTimeBombPreparationView), propertyChanged: OnGamecardChanged);
+
+    static void OnGamecardChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        ExtensionTimeBombPreparationView gamecardView = (ExtensionTimeBombPreparationView)bindable;
+        gamecardView.AnimateCardText();
+    }
+
+    public ExtensionCardDto CurrentCard
+    {
+        get => (ExtensionCardDto)GetValue(CurrentCardProperty);
+        set => SetValue(CurrentCardProperty, value);
+    }
+
+    public static readonly BindableProperty CurrentExtensionProperty = BindableProperty.Create(nameof(CurrentExtension), typeof(Extension), typeof(ExtensionTimeBombPreparationView));
+
+    public Extension CurrentExtension
+    {
+        get => (Extension)GetValue(CurrentExtensionProperty);
+        set => SetValue(CurrentExtensionProperty, value);
+    }
+
+    public static readonly BindableProperty NavigateToSelectCardGameExtensionCommandProperty = BindableProperty.Create(nameof(NavigateToSelectCardGameExtensionCommand), typeof(RelayCommand), typeof(ExtensionTimeBombPreparationView));
+
+    public RelayCommand NavigateToSelectCardGameExtensionCommand
+    {
+        get { return (RelayCommand)GetValue(NavigateToSelectCardGameExtensionCommandProperty); }
+        set { SetValue(NavigateToSelectCardGameExtensionCommandProperty, value); }
+    }
+
+    public static readonly BindableProperty DisplayNextCardCommandProperty = BindableProperty.Create(nameof(DisplayNextCardCommand), typeof(RelayCommand), typeof(ExtensionTimeBombPreparationView));
+
+    public RelayCommand DisplayNextCardCommand
+    {
+        get => (RelayCommand)GetValue(DisplayNextCardCommandProperty);
+        set => SetValue(DisplayNextCardCommandProperty, value);
+    }
+
+    public ExtensionTimeBombPreparationView()
 	{
 		InitializeComponent();
 	}
+
+    public async void AnimateCardText()
+    {
+        await Task.Run(async () =>
+        {
+            var animation = new Animation();
+            var singleAnimationDuration = 1.0 / 2.0;
+
+            for (int i = 0; i < 1; i++)
+            {
+                // pan right
+                animation.Add(i * 2 * singleAnimationDuration, (i * 2 + 1) * singleAnimationDuration, new Animation(v => CardTextStackLayout.TranslationX = v, 0, 15, Easing.CubicOut));
+                // pan left
+                animation.Add((i * 2 + 1) * singleAnimationDuration, (i * 2 + 2) * singleAnimationDuration, new Animation(v => CardTextStackLayout.TranslationX = v, 15, -15, Easing.CubicOut));
+            }
+            // return to starting position
+            animation.Add(1 - singleAnimationDuration, 1, new Animation(v => CardTextStackLayout.TranslationX = v, -15, 0, Easing.CubicOut));
+            animation.Commit(this, "CardTextSwingAnimation", length: 250, easing: Easing.CubicOut);
+        });
+    }
 }
