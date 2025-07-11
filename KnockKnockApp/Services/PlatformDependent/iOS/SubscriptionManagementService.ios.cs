05817c74607108864fa334afc208f8bf6fe439b8
AdrianMrx <adrian.marxreiter@hs-augsburg.de>
2025-07-11 12:35:27 +0200

    remove Plugin.InAppBilling

--- KnockKnockApp/Services/PlatformDependent/iOS/SubscriptionManagementService.ios.cs
+++ KnockKnockApp/Services/PlatformDependent/iOS/SubscriptionManagementService.ios.cs
@@ -1,5 +1,4 @@
 ﻿using KnockKnockApp.Models;
-using Plugin.InAppBilling;
 
 namespace KnockKnockApp.Services
 {
@@ -14,103 +13,104 @@ namespace KnockKnockApp.Services
 
         public partial async Task<bool> PurchaseSubscription()
         {
-            var returnValue = false;
+            return true;
+            //var returnValue = false;
 
-            if (CrossInAppBilling.IsSupported == false)
-            {
-                return returnValue;
-            }
+            //if (CrossInAppBilling.IsSupported == false)
+            //{
+            //    return returnValue;
+            //}
 
-            var billing = CrossInAppBilling.Current;
-            try
-            {
-                var connected = await billing.ConnectAsync();
-                if (!connected)
-                {
-                    return returnValue;
-                }
+            //var billing = CrossInAppBilling.Current;
+            //try
+            //{
+            //    var connected = await billing.ConnectAsync();
+            //    if (!connected)
+            //    {
+            //        return returnValue;
+            //    }
 
-                //purchases
-                var purchase = await billing.PurchaseAsync(productId, ItemType.Subscription);
+            //    //purchases
+            //    var purchase = await billing.PurchaseAsync(productId, ItemType.Subscription);
 
-                //possibility that a null came through.
-                if (purchase != null && purchase.State == PurchaseState.Purchased)
-                {
-                    //only needed on android unless you turn off auto finalize
-                    var ack = await CrossInAppBilling.Current.FinalizePurchaseAsync([purchase.TransactionIdentifier]);
+            //    //possibility that a null came through.
+            //    if (purchase != null && purchase.State == PurchaseState.Purchased)
+            //    {
+            //        //only needed on android unless you turn off auto finalize
+            //        var ack = await CrossInAppBilling.Current.FinalizePurchaseAsync([purchase.TransactionIdentifier]);
 
-                    // Handle if acknowledge was successful or not
-                    if (ack.FirstOrDefault().Success)
-                    {
-                        returnValue = true;
-                    }
-                }
-            }
-            catch (InAppBillingPurchaseException purchaseEx)
-            {
-                //Something has gone wrong, log it
-            }
-            catch (Exception ex)
-            {
-                //Something has gone wrong, log it
-            }
-            finally
-            {
-                await billing.DisconnectAsync();
-            }
+            //        // Handle if acknowledge was successful or not
+            //        if (ack.FirstOrDefault().Success)
+            //        {
+            //            returnValue = true;
+            //        }
+            //    }
+            //}
+            //catch (InAppBillingPurchaseException purchaseEx)
+            //{
+            //    //Something has gone wrong, log it
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Something has gone wrong, log it
+            //}
+            //finally
+            //{
+            //    await billing.DisconnectAsync();
+            //}
 
-            return returnValue;
+            //return returnValue;
         }
 
         public partial async Task UpdateAccountInformation()
         {
-            // AccountInformation.IsPrimeSubscriptionActive = true;
+            AccountInformation.IsPrimeSubscriptionActive = true;
 
-            if (CrossInAppBilling.IsSupported == false)
-            {
-                return;
-            }
+            //if (CrossInAppBilling.IsSupported == false)
+            //{
+            //    return;
+            //}
 
-            var billing = CrossInAppBilling.Current;
-            try
-            {
-                var connected = await billing.ConnectAsync();
-                if (!connected)
-                {
-                    //Couldn't connect
-                    return;
-                }
+            //var billing = CrossInAppBilling.Current;
+            //try
+            //{
+            //    var connected = await billing.ConnectAsync();
+            //    if (!connected)
+            //    {
+            //        //Couldn't connect
+            //        return;
+            //    }
 
-                //get purchase
-                var purchaseList = await billing.GetPurchasesAsync(ItemType.Subscription);
+            //    //get purchase
+            //    var purchaseList = await billing.GetPurchasesAsync(ItemType.Subscription);
 
-                if (purchaseList?.Any(p => p.ProductId == productId) ?? false)
-                {
-                    var sortedPurchaseList = purchaseList.Where(p => p.ProductId == productId).OrderByDescending(i => i.TransactionDateUtc).ToList();
-                    var recentPurchase = sortedPurchaseList[0];
-                    if (recentPurchase != null)
-                    {
-                        if (recentPurchase.TransactionDateUtc.AddDays(10) > DateTime.UtcNow)
-                        {
-                            AccountInformation.IsPrimeSubscriptionActive = true;
-                        }
+            //    if (purchaseList?.Any(p => p.ProductId == productId) ?? false)
+            //    {
+            //        var sortedPurchaseList = purchaseList.Where(p => p.ProductId == productId).OrderByDescending(i => i.TransactionDateUtc).ToList();
+            //        var recentPurchase = sortedPurchaseList[0];
+            //        if (recentPurchase != null)
+            //        {
+            //            if (recentPurchase.TransactionDateUtc.AddDays(10) > DateTime.UtcNow)
+            //            {
+            //                AccountInformation.IsPrimeSubscriptionActive = true;
+            //            }
 
-                        // Für Sanbox-Test (Sandbox weekly ca alle 3min)
-                        //if (recentPurchase.TransactionDateUtc.AddMinutes(10) > DateTime.UtcNow)
-                        //{
-                        //    AccountInformation.IsPrimeSubscriptionActive = true;
-                        //}
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //Something has gone wrong, log it
-            }
-            finally
-            {
-                await billing.DisconnectAsync();
-            }
+            //            // Für Sanbox-Test (Sandbox weekly ca alle 3min)
+            //            //if (recentPurchase.TransactionDateUtc.AddMinutes(10) > DateTime.UtcNow)
+            //            //{
+            //            //    AccountInformation.IsPrimeSubscriptionActive = true;
+            //            //}
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Something has gone wrong, log it
+            //}
+            //finally
+            //{
+            //    await billing.DisconnectAsync();
+            //}
         }
     }
 }
